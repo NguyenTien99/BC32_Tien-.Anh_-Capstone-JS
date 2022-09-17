54139880f7af05dd6d42e4d55d34d1dd79d70002
NguyenTien99 <nguyenmtien0@gmail.com>
2022-09-17 16:48:37 +0700

    complete JS

--- AppWeb/services/productAPI.js
+++ AppWeb/services/productAPI.js
@@ -1,6 +1,9 @@
-function apiGetProducts(){
+function apiGetProducts(searchTerm){
     return axios({
         url: "https://63187fd6ece2736550cba53a.mockapi.io/products",
         method: "GET",
+        params:{
+            type: searchTerm,
+        }    
     })
 }
